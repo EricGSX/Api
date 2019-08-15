f185385c0ae0bb9ce3e7e7ee02371976670a59a7
GuoShiXin <3234655977@qq.com>
2019-08-15 17:35:51 +0800

    移除不用的侧边栏，更新版权信息

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; 2019 <a href="javascript:void(0)">Data Center</a>.</strong> All rights
     reserved.
   </footer>
 <!-- Control Sidebar -->
--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -14,18 +14,6 @@
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">MAIN NAVIGATION</li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-          </ul>
-        </li>
         @if($controllerName == 'SalesController')
               <li class="treeview active">
         @else
@@ -45,40 +33,40 @@
               @endif
           </ul>
         </li>
-        <li class="treeview">
-          <a href="#">
-            <i class="fa fa-share"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-circle-o"></i> Level One
-                <span class="pull-right-container">
-                  <i class="fa fa-angle-left pull-right"></i>
-                </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>
-                <li class="treeview">
-                  <a href="#"><i class="fa fa-circle-o"></i> Level Two
-                    <span class="pull-right-container">
-                      <i class="fa fa-angle-left pull-right"></i>
-                    </span>
-                  </a>
-                  <ul class="treeview-menu">
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                    <li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
-          </ul>
-        </li>
-        <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+        {{--<li class="treeview">--}}
+          {{--<a href="#">--}}
+            {{--<i class="fa fa-share"></i> <span>Multilevel</span>--}}
+            {{--<span class="pull-right-container">--}}
+              {{--<i class="fa fa-angle-left pull-right"></i>--}}
+            {{--</span>--}}
+          {{--</a>--}}
+          {{--<ul class="treeview-menu">--}}
+            {{--<li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>--}}
+            {{--<li class="treeview">--}}
+              {{--<a href="#"><i class="fa fa-circle-o"></i> Level One--}}
+                {{--<span class="pull-right-container">--}}
+                  {{--<i class="fa fa-angle-left pull-right"></i>--}}
+                {{--</span>--}}
+              {{--</a>--}}
+              {{--<ul class="treeview-menu">--}}
+                {{--<li><a href="#"><i class="fa fa-circle-o"></i> Level Two</a></li>--}}
+                {{--<li class="treeview">--}}
+                  {{--<a href="#"><i class="fa fa-circle-o"></i> Level Two--}}
+                    {{--<span class="pull-right-container">--}}
+                      {{--<i class="fa fa-angle-left pull-right"></i>--}}
+                    {{--</span>--}}
+                  {{--</a>--}}
+                  {{--<ul class="treeview-menu">--}}
+                    {{--<li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>--}}
+                    {{--<li><a href="#"><i class="fa fa-circle-o"></i> Level Three</a></li>--}}
+                  {{--</ul>--}}
+                {{--</li>--}}
+              {{--</ul>--}}
+            {{--</li>--}}
+            {{--<li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>--}}
+          {{--</ul>--}}
+        {{--</li>--}}
+        <li><a onclick="alert('敬请期待哦')"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
         <li class="header">LABELS</li>
         <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
         <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>

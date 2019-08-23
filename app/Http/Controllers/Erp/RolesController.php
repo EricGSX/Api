5a902c67e08794d7c309fbc6d9c457a6ec951ba2
GuoShiXin <3234655977@qq.com>
2019-08-23 15:54:43 +0800

    增加了条件判断C

--- app/Http/Controllers/Erp/RolesController.php
+++ app/Http/Controllers/Erp/RolesController.php
@@ -10,16 +10,20 @@ class RolesController extends BaseController
     public function index(Request $request)
     {
         $users = $request->user;
-        $roles = [
-            [
-                'name' => '销量下降',
-                'content' => [['name' => '列表1', 'content' => 'addrole'],['name' => '列表2', 'content' => 'update']],
-            ],
-            [
-                'name' => 'aaa',
-                'content' => [['name' => '列表3', 'content' => 'addrole2'],['name' => '列表4', 'content' => 'update2']],
-            ],
-        ];
+        if(!$users){
+            $roles = [];
+        }else{
+            $roles = [
+                [
+                    'name' => '销量下降',
+                    'content' => [['name' => '列表1', 'content' => 'addrole'],['name' => '列表2', 'content' => 'update']],
+                ],
+                [
+                    'name' => 'aaa',
+                    'content' => [['name' => '列表3', 'content' => 'addrole2'],['name' => '列表4', 'content' => 'update2']],
+                ],
+            ];
+        }
         return view('roles.index',compact('users','roles'));
     }
 

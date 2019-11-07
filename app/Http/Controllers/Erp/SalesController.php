4fd2f8c977feb8b6397f1fad0f078248373b064d
GuoShiXin <3234655977@qq.com>
2019-11-07 14:50:36 +0800

    修改页面

--- app/Http/Controllers/Erp/SalesController.php
+++ app/Http/Controllers/Erp/SalesController.php
@@ -9,28 +9,32 @@ use Illuminate\Support\Facades\DB;
 
 class SalesController extends BaseController
 {
+    /**
+     * 销量下降列表首页
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         BaseController::webAuthTokenCheck();
-        //session::set
-        //$test = DB::connection('sqlsrv')->table('oc_user_info')->where('id','eric.guo')->get();
-        //$test = DB::connection('sqlsrv')->select('select top 1 * from oc_user_info');
-        //dd($test);
-        return view('sales.index');
+        $api = new Api();
+        $dataCenterApi = env('DATACENTER_GetTasks');
+        $user = session('payLoad')['accountName'];
+        $data = [
+            'SearchKey'=>$user,
+            'SearchColumn'=>'OP',
+        ];
+        $tasksData = json_decode($api->httpRequest($dataCenterApi,$data));
+        if(!$tasksData){
+            $data = [
+                'SearchKey'=>'kikki.gu',
+                'SearchColumn'=>'OP',
+            ];
+            $tasksData = json_decode($api->httpRequest($dataCenterApi,$data));
+        }
+        return view('sales.index',compact('tasksData'));
     }
 
-    public function test()
-    {
-        $data = [
-            ['id' => 1,'sku'=>'XXX-XXX-XXX','itemId'=>'123,123,123','type'=>'One'],
-            ['id' => 2,'sku'=>'XXX-XXX-XXX','itemId'=>'123,123,123','type'=>'Two'],
-            ['id' => 3,'sku'=>'XXX-XXX-XXX','itemId'=>'123,123,123','type'=>'Three'],
-            ['id' => 4,'sku'=>'XXX-XXX-XXX','itemId'=>'123,123,123','type'=>'Four'],
-            ['id' => 5,'sku'=>'XXX-XXX-XXX','itemId'=>'123,123,123','type'=>'Five'],
-            ['id' => 6,'sku'=>'XXX-XXX-XXX','itemId'=>'123,123,123','type'=>'Six'],
-        ];
-        return response()->json(['code'=>200,'msg'=>$data]);
-    }
+
 
     public function show(Request $request)
     {
--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -1,10 +1,5 @@
 @extends('layout.main')
 @section('main-content')
-    <style>
-        [v-cloak]{
-            display: none;
-        }
-    </style>
     <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -20,16 +15,6 @@
     </section>
     <section class="content">
       <div class="row" id="app">
-          {{--<section class="col-lg-6">--}}
-              {{--<div class="nav-tabs-custom">--}}
-                {{--<div id="dashboard1" style="width: 100%;height:400px;"></div>--}}
-              {{--</div>--}}
-          {{--</section>--}}
-          {{--<section class="col-lg-6">--}}
-              {{--<div class="nav-tabs-custom">--}}
-                  {{--<div id="dashboard2" style="width: 100%;height:400px;"></div>--}}
-              {{--</div>--}}
-          {{--</section>--}}
           <section class="col-lg-6 connectedSortable">
           <div class="nav-tabs-custom">
                         <iframe src="https://app.powerbi.com/view?r=eyJrIjoiNTlmYjFkNmUtYjNkZS00ZjU2LTg5MGEtNmM5OTEwMDM1MjAzIiwidCI6IjdlNGFiZmYxLWU3MjEtNGZhZC04MTdlLTZjZDI0NmIyNzgxNSIsImMiOjZ9" width="100%" height="400">
@@ -48,28 +33,34 @@
         </section>
         <section class="col-lg-12">
           <div class="nav-tabs-custom">
-              <p>Amazon 销量下滑数据</p>
+              <p>销量下滑数据</p>
               <table class="table table-hover">
                   <thead>
                     <tr>
-                      <th>#</th>
+                      {{--<th>#</th>--}}
                       <th>SKU</th>
                       <th>ItemId</th>
+                      <th>Platform</th>
                       <th>Type</th>
+                      <th>OP</th>
                       <th>Handle</th>
                     </tr>
                   </thead>
                   <tbody id="loadTbody">
-                    <tr v-for="site in dataLists" v-cloak>
-                      <th scope="row">@{{site.id}}</th>
-                      <td>@{{ site.sku }}</td>
-                      <td>@{{ site.itemId }}</td>
-                      <td>@{{ site.type }}</td>
+                  @foreach ($tasksData as $tasks)
+                    <tr>
+                      {{--<th>{{$tasks->ID}}</th>--}}
+                      <td>{{$tasks->Sku}}</td>
+                      <td>{{$tasks->ItemId}}</td>
+                      <td>{{$tasks->Platform}}</td>
+                      <td>{{$tasks->Type}}</td>
+                      <td>{{$tasks->OP}}</td>
                       <td>
-                          <button typt="button"  class="btn btn-sm btn-warning" @click="report(site.id)">报表</button>
-                          <button typt="button" class="btn btn-sm btn-primary" @click="myFT()">操作</button>
+                          <button typt="button"  class="btn btn-sm btn-warning" onclick="report('{{$tasks->ID}}')">报表</button>
+                          <button typt="button" class="btn btn-sm btn-primary" onclick="myFT()">操作</button>
                       </td>
                     </tr>
+                  @endforeach
                   </tbody>
                 </table>
           </div>
@@ -83,14 +74,6 @@
                 <h4 class="modal-title">操作</h4>
               </div>
               <div class="modal-body">
-                {{--<div class="col-md-12">--}}
-                    {{--<div class="col-md-3">--}}
-                        {{--Lable1--}}
-                    {{--</div>--}}
-                    {{--<div class="col-md-9">--}}
-                        {{--<input type="text" class="form-control" v-model="label1">--}}
-                    {{--</div>--}}
-                {{--</div>--}}
 
                   {{--正式操作信息Start--}}
                   <div class="col-md-12 form-group">
@@ -187,152 +170,15 @@
     <!-- /.content -->
   </div>
 <script>
-window.onload=function(){
-    var option = {
-        title : {
-            text: 'Amazon销量下降报表',
-            x:'center'
-        },
-        tooltip : {
-            trigger: 'item',
-            formatter: "{a} <br/>{b} : {c} ({d}%)"
-        },
-        legend: {
-            orient: 'vertical',
-            left: 'left',
-            data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-        },
-        series : [
-            {
-                name: '访问来源',
-                type: 'pie',
-                radius : '55%',
-                center: ['50%', '60%'],
-                data:[
-                    {value:335, name:'直接访问'},
-                    {value:310, name:'邮件营销'},
-                    {value:234, name:'联盟广告'},
-                    {value:135, name:'视频广告'},
-                    {value:1548, name:'搜索引擎'}
-                ],
-                itemStyle: {
-                    emphasis: {
-                        shadowBlur: 10,
-                        shadowOffsetX: 0,
-                        shadowColor: 'rgba(0, 0, 0, 0.5)'
-                    }
-                }
-            }
-        ]
-    };
-    var option2 = {
-        title: {
-            text: 'Amazon销量下降来源',
-        },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data:['邮件营销','联盟广告','视频广告','直接访问','搜索引擎']
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一','周二','周三','周四','周五','周六','周日']
-        },
-        yAxis: {
-            type: 'value'
-        },
-        series: [
-            {
-                name:'邮件营销',
-                type:'line',
-                stack: '总量',
-                data:[120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-                name:'联盟广告',
-                type:'line',
-                stack: '总量',
-                data:[220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-                name:'视频广告',
-                type:'line',
-                stack: '总量',
-                data:[150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-                name:'直接访问',
-                type:'line',
-                stack: '总量',
-                data:[320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-                name:'搜索引擎',
-                type:'line',
-                stack: '总量',
-                data:[820, 932, 901, 934, 1290, 1330, 1320]
-            }
-        ]
-    };
+    function report(id){
+    console.log(123123);
+    console.log(id);
+        window.location.href="/sales/declineDetail?id="+id;
+    }
 
-    $.get('/api/test',{},function(msg){
-      var vm =   new Vue({
-            el: '#app',
-            data: {
-                dataLists: msg.msg,
-                chart: null,
-                chart2: null,
-                EricTest: null,
-                label1:'',
-                youhua:'',
-                tiaozheng:'',
-            },
-            mounted:function(){
-                this.drawLine();
-            },
-            methods:{
-                drawLine:function(){
-                        this.chart = echarts.init(document.getElementById('dashboard1'));
-                        this.chart.setOption(option);
-                        this.chart2 = echarts.init(document.getElementById('dashboard2'));
-                        this.chart2.setOption(option2);
-                    },
-                myFT:function(){
-                    this.EricTest = 'Hello World';
-                    $('#skuHandle').modal('show')
-//                    var test = [
-//                        { id: 'Runoob' },
-//                        { id: 'Google' },
-//                        { id: 'Taobao' }
-//                    ];
-//                    this.dataLists = test;
-                },
-                report:function(id){
-//                    console.log(id);
-//                    $.get('/sales/declineDetail',{'id':id},function(re){
-//                        console.log(re)
-//                    })
-                    window.location.href="/sales/declineDetail?id=1";
-                },
-                skuUp:function(){
-                    alert(this.youhua);
-                }
-            }
-        })
-    });
-
-
-
-}
-
+    function myFT(){
+        $('#skuHandle').modal('show')
+    }
 
 </script>
 @endsection
